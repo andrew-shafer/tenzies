d7a5e8bb86b5a57e21e75478de0df034afed0549
Andrew Shafer <andrewshafer@Andrews-MacBook-Pro.local>
2025-08-04 22:08:21 -0400

    End game feature changes

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,17 +1,36 @@
 import Die from "../components/Die.jsx"
-import {useState} from "react"
+import { useState } from "react"
+import { useWindowSize } from 'react-use'
+import Confetti from 'react-confetti'
 import { nanoid } from "nanoid"
-/**
-     * Challenge: Update the `hold` function to flip
-     * the `isHeld` property on the object in the array
-     * that was clicked, based on the `id` prop passed
-     * into the function.
-     * 
-     * Hint: as usual, there's more than one way to 
-     * accomplish this.
-     */ 
     
+/**
+     * Challenge part 2:
+     * 1. Create a new `gameWon` variable.
+     * 2. If `gameWon` is true, change the button text to
+     *    "New Game" instead of "Roll"
+     */
+
 export default function App() {
+    const [diceArray, setDiceArray] = useState(generateAllNewDice());
+    const dieComponentArray = diceArray.map((die) => {
+        return (
+            <Die  
+                key={die.id} 
+                id={die.id}
+                value={die.value} 
+                isHeld={die.isHeld}
+                holdFunction={() => hold(die.id)}
+            />
+        )
+    });
+    let gameWon = false;
+
+    if (diceArray.every(die => die.isHeld === true) &&
+        diceArray.every(die => die.value === diceArray[0].value) ) {
+        gameWon = true;
+    }
+
     function generateAllNewDice() {
         const diceArray = [];
         for (let i=0; i<10; i++) {
@@ -24,34 +43,29 @@ export default function App() {
     }
 
     function reRollDice() {
-        setDiceArray(prevDiceArray => (generateAllNewDice()));
+        setDiceArray(prevDiceArray => prevDiceArray.map(die => {
+            return die.isHeld === false ? {...die, value:1+Math.floor(Math.random()*6)} : die
+        }));
     }
 
     function hold(id) {
-        setDiceArray((prevDiceArray) => {
-            return prevDiceArray.map((die) => {
-                return id === die.id ? {...die, isHeld: !die.isHeld} :
-                                        die
+        setDiceArray(prevDiceArray => 
+            prevDiceArray.map(die => {
+                return id === die.id ? 
+                    {...die, isHeld: !die.isHeld} : die
             })
-        })
+        )
     }
 
-    const [diceArray, setDiceArray] = useState(generateAllNewDice());
-    const dieComponentArray = diceArray.map((die) => {
-        return (
-        <Die  
-            key={die.id} 
-            id={die.id}
-            value={die.value} 
-            isHeld={die.isHeld}
-            holdFunction={hold}/>)
-    });
     return (
         <main>
+            {gameWon && <Confetti />}
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
                 {dieComponentArray}
             </div>
-            <button id="roll" onClick={reRollDice}>Roll</button>
+            <button id="roll" onClick={reRollDice}>{gameWon ? "New Game" : "Roll"}</button>
         </main>
     )
 }
